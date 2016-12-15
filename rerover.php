42d9dcc6a9983e05d3ecce62d7d180a312e162a4
SpeaKeasY <speakeasysky@gmail.com>
2016-12-15 16:05:20 +0000

    Fixed typos/errors, working copy. less threads

--- rerover.php
+++ rerover.php
@@ -2,7 +2,7 @@
 
 class rerover extends Thread {
 
-    public function __construct($i = 0, $j = 0) {
+    public function __construct($i = 0) {
         $this->i = $i;
         $this->j = $j;
     }
@@ -12,7 +12,7 @@ class rerover extends Thread {
         $this->r_view("14816716305193269493092739739619", $this->i, $this->j);
     }
 
-    public function r_view($key = "14816716305193269493092739739619", $i = 0, $j = 0) {
+    public function r_view($key = "14816716305193269493092739739619", $i = 0) {
         $opts = array(
             'http' => array(
                 'method' => "GET",
@@ -23,20 +23,26 @@ class rerover extends Thread {
 
         echo "Started.\r\n";
         $int8 = $i;
-        $int7 = $j;
-        for ($int6 = 0; $int6 < 10; $int6++) {
-            for ($int5 = 0; $int5 < 10; $int5++) {
-                for ($int4 = 0; $int4 < 10; $int4++) {
-                    for ($int3 = 0; $int3 < 10; $int3++) {
-                        for ($int2 = 0; $int2 < 10; $int2++) {
-                            for ($int1 = 0; $int1 < 10; $int1++) {
-                                $xmlstr = file_get_contents("http://rover.info/r_view/" . $int8 . $int7 . $int6 . $int5 . $int4 . $int3 . $int2 . $int1, false, $context);
-                                try {$xml = new SimpleXMLElement($xmlstr); } catch (Exception $ex) {}
-                                //$xml = (Array) $xml;
-                                //print_r($xml);
-                                if ($this->xml_attribute($xml, 'exist')) {
-                                    file_put_contents("r_view-" . $int8 . $int7 . $int6 . $int5 . $int4 . $int3 . $int2 . $int1 . ".xml", $xmlstr);
-                                    echo "Saved: r_view-" . $int8 . $int7 . $int6 . $int5 . $int4 . $int3 . $int2 . $int1 . ".xml\n";
+        for ($int7 = 0; $int7 < 10; $int7++) {
+            for ($int6 = 0; $int6 < 10; $int6++) {
+                for ($int5 = 0; $int5 < 10; $int5++) {
+                    for ($int4 = 0; $int4 < 10; $int4++) {
+                        for ($int3 = 0; $int3 < 10; $int3++) {
+                            for ($int2 = 0; $int2 < 10; $int2++) {
+                                for ($int1 = 0; $int1 < 10; $int1++) {
+                                    $xmlstr = file_get_contents("http://rover.info/r_view/" . $int8 . $int7 . $int6 . $int5 . $int4 . $int3 . $int2 . $int1, false, $context);
+
+                                    try {
+                                        $xml = new SimpleXMLElement($xmlstr);
+                                    } catch (Exception $ex) {
+                                        
+                                    }
+                                    //$xml = (Array) $xml;
+                                    //print_r($xml);
+                                    if ($this->xml_attribute($xml, 'exist')) {
+                                        file_put_contents("r_view-" . $int8 . $int7 . $int6 . $int5 . $int4 . $int3 . $int2 . $int1 . ".xml", $xmlstr);
+                                        echo "Saved: r_view-" . $int8 . $int7 . $int6 . $int5 . $int4 . $int3 . $int2 . $int1 . ".xml\n";
+                                    }
                                 }
                             }
                         }
@@ -54,8 +60,6 @@ class rerover extends Thread {
 }
 
 for ($i = 0; $i < 10; $i++) {
-    for ($j = 0; $j < 10; $j++) {
-        $workers[(int)$i.$j] = new rerover($i, $j);
-        $workers[(int)$i.$j]->start();
-    }
+        $workers[$i] = new rerover($i);
+        $workers[$i]->start();
 }
